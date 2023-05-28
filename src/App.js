3067e9bda85e44024b661cc7159a9b60fe746783
Michal Olszewski <m.olszewski0808@gmail.com>
2023-05-28 20:08:49 +0200

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  }
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -31,9 +41,11 @@ function App() {
       />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks}
+        body={<Tasks
+          tasks={tasks}
           hideDone={hideDone}
           removeTask={removeTask}
+          toggleTaskDone={toggleTaskDone}
         />}
         extraHeaderContent=
         {<Buttons
